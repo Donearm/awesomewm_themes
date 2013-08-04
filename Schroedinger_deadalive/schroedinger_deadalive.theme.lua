c0e0ef621fb1ca0d7e2c799c9bc98ab4d94e9678
Gianluca Fiore <forod.g@gmail.com>
2013-08-04 17:46:30 +0200

    Add theme.wallpaper variable to Schroedinger_deadalive

--- Schroedinger_deadalive/schroedinger_deadalive.theme.lua
+++ Schroedinger_deadalive/schroedinger_deadalive.theme.lua
@@ -75,7 +75,8 @@ theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
--- You can use your own command to set your wallpaper
+
+theme.wallpaper = "/mnt/documents/Images/schroedinger_deadalive.jpg"
 theme.wallpaper_cmd = { "feh --bg-scale /mnt/documents/Images/schroedinger_deadalive.jpg" }
 
 -- You can use your own layout icons like this:
